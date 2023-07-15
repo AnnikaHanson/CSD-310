be55206c72a8b72f5a703c1974c2eb5591269097
AnnikaHanson <ahanson@my365.bellevue.edu>
2023-07-15 15:56:24 -0500

    Push

--- Module_8/pysports_queries.py
+++ Module_8/pysports_queries.py
@@ -1,22 +1,18 @@
-import _mysql_connector
-from _mysql_connector import Error
+import mysql.connector
+from mysql.connector import Error
 
-try: 
-    connection = _mysql_connector.connect(host='localhost',
-database='teams', 
+connection = mysql.connector.connect(host="localhost",
+database='pysports', 
 user='root',
 password='ntA6KNFQGBZeRv2rVT7K')
     
-     if connection.is_connected():
+if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
         cursor = connection.cursor()
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("You're connected to database: ", record)
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
 
 
     ##Query##
